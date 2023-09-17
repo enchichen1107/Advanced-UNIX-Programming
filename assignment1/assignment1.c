a42496f68fdefe17905af62e5384b07b35cc319f
enchichen1107 <b03409032@gmail.com>
2023-09-17 21:51:59 +0800

    fix git4

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -7,25 +7,25 @@
 int main()
 {
   int fd, n;
-  int offset = sizeof("student.")-1;
+  int offset = sizeof("student.");
   char buffer[100];
   char change_line[] = "\n";
   char replaced_str[] = "NTHU student.";
 
-// print "student."
-  fd = open("sample.txt", O_RDWR|O_APPEND);
+  // print "student."
+  fd = open("sample.txt", O_RDWR);
   lseek(fd, -offset, SEEK_END);
   read(fd, buffer, offset);
   write(1, buffer, offset);
 
-// change line
-  write(1, change_line, 2);
+  // change line only needed when test under mac osx
+  // write(1, change_line, 2);
 
-// write replaced string to sample.txt 
+  // write replaced string to sample.txt
   lseek(fd, -offset, SEEK_END);
-  write(fd, replaced_str, sizeof(replaced_str)-1);
+  write(fd, replaced_str, sizeof(replaced_str));
 
-// print sample.txt
+  // print sample.txt
   lseek(fd, 0, SEEK_SET);
   n = read(fd, buffer, 100);
   write(1, buffer, n);
